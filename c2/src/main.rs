f5b3683b4f84bacf413a1f2d95ccf780401cfd48
n0op3 <n0op3@proton.me>
2025-08-30 17:29:39 +0200

    hello network

--- c2/src/main.rs
+++ c2/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io::Read, net::TcpListener};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("0.0.0.0:9120")?;
+
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+
+        let mut buf = vec![0; 1024];
+
+        stream.read(&mut buf).expect("failed to read from stream");
+
+        println!("{}", String::from_utf8_lossy(&buf));
+    }
+
+    Ok(())
 }
--- implant/src/main.rs
+++ implant/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io::Write, net::TcpStream};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut connection = TcpStream::connect("127.0.0.1:9120")?;
+
+    connection.write("Hello, world!".as_bytes())?;
+
+    Ok(())
 }
